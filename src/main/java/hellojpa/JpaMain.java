834b64e52341abae2ab01fc5784f4f7f4db57382
DonghyunShin <shindh0429@gmail.com>
2022-06-26 23:56:53 +0900

    Basic Usage

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -16,16 +16,22 @@ public class JpaMain {
         //Transaction 호출
         tx.begin();
         //Transaction 시작
-        //실제 동작 코드 삽입
-        Member member = new Member();
-        member.setId(2L);
-        member.setName("Hello!");
-        em.persist(member);
-
-        tx.commit();
-        //저장
-        em.close();
-        //emf 종료
+        try {
+            //실제 동작 코드 삽입
+            Member member = new Member();
+            member.setId(2L);
+            member.setName("Hello!");
+            em.persist(member);
+            tx.commit();
+            //저장
+        }
+        catch (Exception e){
+            tx.rollback();
+        }
+        finally {
+            em.close();
+            //EntityManeger 종료 *사용후에 꼭 닫아줘야 함.
+        }
         emf.close();
         //Application 완전 종료
     }
