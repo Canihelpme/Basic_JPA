2a2b2e9b268967472d0a21448d2953289d55996c
DonghyunShin <shindh0429@gmail.com>
2022-06-26 23:52:04 +0900

    Basic

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -2,6 +2,7 @@ package hellojpa;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
 public class JpaMain {
@@ -10,7 +11,19 @@ public class JpaMain {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
         //emf 생성
         EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        //Transaction 호출
+        tx.begin();
+        //Transaction 시작
         //실제 동작 코드 삽입
+        Member member = new Member();
+        member.setId(2L);
+        member.setName("Hello!");
+        em.persist(member);
+
+        tx.commit();
+        //저장
         em.close();
         //emf 종료
         emf.close();
